daff71ef822098c82b0cfb7e8fca2c1d79bb588c
Pavel Dzunovich <pavel_dzunovich@epam.com>
2017-09-16 14:39:04 +0300

    Fix collection removal
    - since operation is async on mongo instance level,
      have to introduce more safe waiting

--- src/main/java/by/segg3r/testng/util/mongo/MongoStartupListener.java
+++ src/main/java/by/segg3r/testng/util/mongo/MongoStartupListener.java
@@ -6,10 +6,16 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.testng.IExecutionListener;
 import org.testng.ITestResult;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @ContextConfiguration(configClasses = {MongoInstanceConfiguration.class})
 public class MongoStartupListener implements IExecutionListener, SuiteTestListener {
 
 	private static final String SYSTEM_COLLECTION_PREFIX = "system";
+	private static final long COLLECTION_REMOVAL_TIMEOUT = 60 * 1000; //60s
+	private static final long COLLECTION_REMOVAL_SLEEP = 10; //10ms
 
 	@Override
 	public void onExecutionStart() {
@@ -24,11 +30,34 @@ public class MongoStartupListener implements IExecutionListener, SuiteTestListen
 	@Override
 	public void onAfterSuiteTest(Object suite, ITestResult testResult) {
 		MongoTemplate template = MongoInstance.get();
-		for (String collectionName : template.getDb().getCollectionNames()) {
-			if (!collectionName.startsWith(SYSTEM_COLLECTION_PREFIX)) {
-				template.dropCollection(collectionName);
+		getTestCollectionNames().forEach(template::dropCollection);
+
+		waitTestCollectionsRemoved();
+	}
+
+	private void waitTestCollectionsRemoved() {
+		long start = System.currentTimeMillis();
+		while (!getTestCollectionNames().isEmpty()) {
+			try {
+				Thread.sleep(COLLECTION_REMOVAL_SLEEP);
+			} catch (InterruptedException e) {
+				throw new RuntimeException(
+						"Sleep was interrupted while waiting for test collections to be removed.");
+			}
+
+			long now = System.currentTimeMillis();
+			if (now - start >= COLLECTION_REMOVAL_TIMEOUT) {
+				throw new RuntimeException("Test collections were not removed in timeout: "
+						+ COLLECTION_REMOVAL_TIMEOUT);
 			}
 		}
+	}
+
+	private List<String> getTestCollectionNames() {
+		MongoTemplate template = MongoInstance.get();
+		return template.getDb().getCollectionNames().stream()
+				.filter(collection -> !collection.startsWith(SYSTEM_COLLECTION_PREFIX))
+				.collect(toList());
 	}
 
 }
